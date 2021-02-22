d94b4f7e16448ff7f1919e2ce32e8d3109d121af
Javier Manzano <javier.manzano22@gmail.com>
2021-02-22 20:02:01 +0100

    fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import { useFormik } from 'formik';
+import * as Yup from 'yup';
 
 import './App.css';
 import Welcome from './steps/Welcome';
@@ -14,6 +15,18 @@ interface FormValues {
   providers: Array<string>,
 };
 
+const DataCollectionSchema = Yup.object().shape({
+  firstName: Yup.string()
+    .min(2, 'Too Short!')
+    .max(50, 'Too Long!')
+    .required('Required'),
+  lastName: Yup.string()
+    .min(2, 'Too Short!')
+    .max(50, 'Too Long!')
+    .required('Required'),
+  email: Yup.string().email('Invalid email').required('Required'),
+});
+
 function App() {
   const [step, setStep] = useState(0);
   const initialValues: FormValues = {
@@ -24,16 +37,22 @@ function App() {
   };
   const formik = useFormik({
     initialValues,
+    validationSchema: DataCollectionSchema,
     onSubmit: values => {
       alert(JSON.stringify(values, null, 2));
     },
   });
   const values = formik?.values;
 
+  console.log({ errors: formik?.errors });
+  console.log({ touched: formik?.touched });
   const STEPS = [
     <Welcome
       onNext={() => setStep(step + 1)} />,
     <UserInfo
+      values={formik.values}
+      errors={formik.errors}
+      touched={formik.touched}
       onNext={() => {
         if (!values.firstName || !values.lastName && !values.email) {
           return;
--- src/steps/UserInfo.tsx
+++ src/steps/UserInfo.tsx
@@ -1,12 +1,17 @@
-import React from 'react';
-import { Container, Title, Button, FormLayout, FormRow, Label, Input } from '../common-styles';
+import { values } from 'lodash';
+import React, { useState } from 'react';
+import { Container, Title, Button, FormLayout, FormRow, Label, Input, ErrorLabel } from '../common-styles';
 
 type Props = {
+  values: any;
+  errors: any;
+  touched: any;
   onNext: (...args: any[]) => any;
   handleChange: (...args: any[]) => any;
 };
 
-function UserInfo({ onNext, handleChange }: Props) {
+function UserInfo({ onNext, handleChange, values, errors }: Props) {
+  const [isSubmitted, setSubmitted] = useState(false);
   return (
     <Container>
       <Title>
@@ -17,18 +22,24 @@ function UserInfo({ onNext, handleChange }: Props) {
         <FormRow>
           <Label>What's your first name?</Label>
           <Input name="firstName" id="firstName" type="text" onChange={handleChange} />
+          {errors?.firstName && <ErrorLabel>{errors?.firstName}</ErrorLabel>}
         </FormRow>
         <FormRow>
           <Label>What's your last name?</Label>
           <Input name="lastName" id="lastName" type="text" onChange={handleChange} />
+          {errors?.firstName && <ErrorLabel>{errors?.lastName}</ErrorLabel>}
         </FormRow>
         <FormRow>
           <Label>What's your email?</Label>
           <Input name="email" id="email" type="email" onChange={handleChange} />
+          {errors?.email && <ErrorLabel>{errors?.email}</ErrorLabel>}
         </FormRow>
       </FormLayout>
 
-      <Button onClick={() => onNext()}>Next</Button>
+      <Button onClick={() => {
+        setSubmitted(true);
+        onNext();
+      }}>Next</Button>
     </Container>
   );
 }
